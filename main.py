3b9d7c3182843eeb22b3a534a0dc27806449e354
RockyRoad000 <93890750+RockyRoad000@users.noreply.github.com>
2021-11-08 11:05:32 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+a = int(input("enter the first number, \"a\""))
+b = int(input("enter the second number, \"b\""))
+c = int(input("enter the third number, \"c\""))
+print(f"is \"a\" equal to \"b\"?",{a==b})
+print(f"is \"a\" greater than \"b\"?",{a>b})
+print(f"is \"a\" less than \"b\"?",{a<b},"\n")
+
+print(f"is \"a\" equal to \"c\"?",{a==c})
+print(f"is \"a\" greater than \"c\"?",{a>c})
+print(f"is \"a\" less than \"c\"?",{a<c},"\n")
+
+print(f"is \"b\" equal to \"c\"?",{b==c})
+print(f"is \"b\" greater than \"c\"?",{b>c})
+print(f"is \"b\" less than \"c\"?",{b<c},"\n")
