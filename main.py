a63fe4d3d88038eaee3f248326de038df3da623a
RockyRoad000 <93890750+RockyRoad000@users.noreply.github.com>
2021-11-08 11:07:12 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+a = 30000
+a = int(a)
+print("number is " + str(a))
+a = a/1000;
+print("calculation #1 produces " + str(a));
+a = a**3;
+print("calculation #2 produces " + str(a));
+a = a%9001
+print("calculation #3 produces " + str(a));
+a+=2
+print("calculation #4 produces " + str(a));
+a/=3000
+print("calculation #5 produces " + str(a));
+a*=111
+print("calculation #6 produces " + str(a));
