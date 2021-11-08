3164081d79119c1766fdbd5d3003c234b536f48c
RockyRoad000 <93890750+RockyRoad000@users.noreply.github.com>
2021-11-08 11:01:47 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 number = 0
 sum = 0
-for number in range(1,101):
-    print(number+1)
+for number in range(10,21,2):
+    print("," + str(number), end=" ")
     sum+=number
-    print("sum is " + str(sum))
+print("sum is " + str(sum))
+print("\n")
+number = 0
+sum = 0
+for number in range(50,0,-5):
+    print("," + str(number), end=" ")
+    sum+=number
+print("sum is " + str(sum))
