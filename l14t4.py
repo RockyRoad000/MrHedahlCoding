6f88a02ec948ed727a4299ac5d26d6e4b04dfacf
RockyRoad000 <93890750+RockyRoad000@users.noreply.github.com>
2022-02-08 08:00:00 -0800

    Create l14t4.py

---
+++ l14t4.py
@@ -0,0 +1,8 @@
+mysentence = "When it is hot , I like to swim at Lake Whatcom.";
+print(mysentence);
+newsentece = mysentence.replace("hot", "cold");
+mysentence = newsentece.replace("Lake Whatcom", "Mount Baker");
+print(mysentence);
+newsentence = " There are also other fun things to do.";
+mysentence+=newsentence;
+print(mysentence);
